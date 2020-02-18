c38bc45f0f6346e255605b41d85247ad7b2e33a5
Massiel50 <massielescano@yahoo.com>
2020-02-18 13:27:17 -0500

    deployed app to git

---
+++ build/precache-manifest.adc2576bab50042e7b7a89ee48854a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9c7799b18b4c8411e22fc4b9833852",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "7440877b00ba4b4b37f9",
+    "url": "/Clicky/static/css/main.0178515e.chunk.css"
+  },
+  {
+    "revision": "06d8c545ec8809abbd47",
+    "url": "/Clicky/static/js/2.34ccbad4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky/static/js/2.34ccbad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7440877b00ba4b4b37f9",
+    "url": "/Clicky/static/js/main.101da5ca.chunk.js"
+  },
+  {
+    "revision": "9a35025b8cac0f00b9fb",
+    "url": "/Clicky/static/js/runtime-main.94019b1a.js"
+  }
+]);
